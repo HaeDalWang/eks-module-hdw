75c5d4661447879d20e8797b90aaa7753e4bf87f
EC2 Default User <ec2-user@ip-10-50-60-210.ap-northeast-2.compute.internal>
2024-07-16 14:21:59 +0900

    복사용 모듈 삭제

--- module/kubernetes/eks-irsa/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "serviceaccount_name" {
-  description = "생성된 서비스 어카운트 이름"
-  value       = !var.create_iam_role_only ? kubernetes_service_account.this[0].metadata[0].name : null
-}
-
-output "role_arn" {
-  description = "서비스 어카운트에 연동된 IAM 역할 ARN"
-  value       = aws_iam_role.this.arn
-}
--- module/kubernetes/eks-ng/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "node_role" {
-  description = "노드그룹에 부여된 IAM 역할"
-  value       = aws_iam_role.this.name
-}
